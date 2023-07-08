603e41aa5bd8ec7b5f5b5677140c871016349894
chonger878 <echong06@gmail.com>
2023-07-08 02:02:22 -0700

    Added route to server.py file

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask_app import app
+from flask import render_template
+
+@app.route('/')
+def login():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
